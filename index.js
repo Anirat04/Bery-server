062d13e33b76986228c890bec8876d0a86038525
Tanbir Haque <tanbirhaque53@gmail.com>
2023-11-27 06:21:09 +0600

    Jwt completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -34,15 +35,41 @@ async function run() {
     const wishlistCollection = client.db('Bery_DB').collection('wishlist')
     const propertyBoughtCollection = client.db('Bery_DB').collection('property_bought')
 
+    // JWT related API
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({ message: 'forbidden access'})
+      }
+      const token =  req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'forbidden  access'})
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+
+
+
     // Users related API
-    app.get('/allUsers', async (req, res) => {
+    app.get('/allUsers', verifyToken, async (req, res) => {
+
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
 
     app.get('/users', async (req, res) => {
       const userEmail = req.query.email
-      const query = {email: userEmail}
+      const query = { email: userEmail }
       const result = await usersCollection.find(query).toArray()
       res.send(result)
     })
@@ -51,18 +78,18 @@ async function run() {
       const userInfo = req.body
       // inserrt email if user doesn't exist:
       // you can do this many ways (1. unique email, 2. Upsert 3.simple checking)
-      const query = {email: userInfo.email}
+      const query = { email: userInfo.email }
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedID: null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedID: null })
       }
       const result = await usersCollection.insertOne(userInfo)
       res.send(result)
     })
     // user admin role Control API
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
         $set: {
           role: 'admin'
@@ -72,9 +99,9 @@ async function run() {
       res.send(result)
     })
     // user Agent role Control API
-    app.patch('/users/agent/:id', async(req, res) => {
+    app.patch('/users/agent/:id', async (req, res) => {
       const id = req.params.id
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
         $set: {
           role: 'agent'
@@ -86,7 +113,7 @@ async function run() {
     // user Delete button control API
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await usersCollection.deleteOne(query)
       res.send(result)
     })
